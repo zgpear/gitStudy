88452b051127ae5c95c4319b346f55ce865486e5
zc <zcpear@163.com>
2020-03-05 14:59:00 +0800

    first commit

---
+++ 01.js
@@ -0,0 +1,29 @@
+/*
+声明变量let和const
+*/
+//var flag = 123;
+//console.log(flag);
+
+//let flag = 123
+var flag = 123;
+var flag = 456;
+console.log(flag)
+if(true){
+	let flag = 123;
+	console.log(flag);
+
+}
+const n = 1;
+console.log(n);
+console.log('hello kitty')
+
+function sum(n){
+	var sum = 0;
+	for (var i = 0;i<=n;i++){
+		sum += i;
+	}
+	return sum;
+}
+var ret = sum(100);
+console.log(ret);
+console.log(process.arch)
